7a5d0cabcda2d952c9220b03c00d41c60415e8a6
luis daniel <luisdanielrovira8@gmail.com>
2018-05-10 00:28:18 -0400

    add chips

---
+++ docs/.vuepress/dist/assets/js/26.714a0be6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{213:function(n,t,e){},273:function(n,t,e){"use strict";var s=e(213);e.n(s).a},383:function(n,t,e){"use strict";e.r(t);var s={},i=(e(273),e(0)),o=Object(i.a)(s,function(){var n=this.$createElement;return(this._self._c||n)("div",{},[this._v("\n  hola soy el demo\n")])},[],!1,null,null,null);t.default=o.exports}}]);
