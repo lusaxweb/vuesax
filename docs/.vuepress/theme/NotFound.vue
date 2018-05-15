b8d1c075e830c604d5a5097833cdb9ab07051d52
luis daniel <luisdanielrovira8@gmail.com>
2018-05-15 11:02:43 -0400

    add 404 svg

--- docs/.vuepress/theme/NotFound.vue
+++ docs/.vuepress/theme/NotFound.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="theme-container">
-    <div class="content">
-      <h1>404</h1>
-      <blockquote>{{ getMsg() }}</blockquote>
+    <div class="content-notfound">
+      <div class="con-404">
+        <a href="https://lusaxweb.github.io/vuesax/">
+          <not-found/>
+        </a>
+      </div>
+      <!-- <h1>404</h1>
+      <blockquote>{{ getMsg() }}</blockquote> -->
     </div>
   </div>
 </template>
 
 <script>
+import notFound from './notFound404.vue'
 const msgs = [
   `There's nothing here.`,
   `How did we get here?`,
@@ -16,6 +22,9 @@ const msgs = [
 ]
 
 export default {
+  components:{
+    notFound
+  },
   methods: {
     getMsg () {
       return msgs[Math.floor(Math.random() * msgs.length)]
@@ -23,3 +32,20 @@ export default {
   }
 }
 </script>
+
+<style lang="css">
+  .content-notfound {
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+  }
+  .con-404 {
+    position: relative;
+    display: block;
+    width: 100%;
+  }
+  .con-404 svg {
+    margin: auto;
+    display: block;
+  }
+</style>
