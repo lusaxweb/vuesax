3b31c1ca4c8775ba46b788e8dc282faa82c0ad65
luis daniel <luisdanielrovira8@gmail.com>
2018-05-15 10:20:22 -0400

    add docs Avatar

---
+++ docs/.vuepress/dist/assets/js/33.93021ca6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{242:function(n,t,e){},313:function(n,t,e){"use strict";var s=e(242);e.n(s).a},444:function(n,t,e){"use strict";e.r(t);var s={},i=(e(313),e(0)),o=Object(i.a)(s,function(){var n=this.$createElement;return(this._self._c||n)("div",{},[this._v("\n  hola soy el demo\n")])},[],!1,null,null,null);t.default=o.exports}}]);
