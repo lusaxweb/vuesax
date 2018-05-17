05bdbe101543a87afb21d4d5dae0b03e548908e7
luis daniel <luisdanielrovira8@gmail.com>
2018-05-16 23:25:58 -0400

    add docs pagination

---
+++ docs/.vuepress/dist/assets/js/35.a5bf2d5d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{270:function(n,t,e){},344:function(n,t,e){"use strict";var s=e(270);e.n(s).a},481:function(n,t,e){"use strict";e.r(t);var s={},i=(e(344),e(0)),o=Object(i.a)(s,function(){var n=this.$createElement;return(this._self._c||n)("div",{},[this._v("\n  hola soy el demo\n")])},[],!1,null,null,null);t.default=o.exports}}]);
