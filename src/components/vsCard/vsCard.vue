56090aa9bf09dbf8e6d0060c4804e759a0fd951a
Rodman Swanston <swanston.rod@gmail.com>
2018-05-22 14:38:39 -0400

    Added card hover effect and CardHeader child component

--- src/components/vsCard/vsCard.vue
+++ src/components/vsCard/vsCard.vue
@@ -2,6 +2,7 @@
   <div
     @click="clickCard"
     class="vs-card"
+    v-bind:class="{ actionable }"
     :style="{
       /* 'border-color':vsColor?/[#()]/.test(vsColor)?vsColor:`rgba(var(--${vsColor}),1)`:'rgb(255,255,255)' */
     }">
@@ -17,6 +18,10 @@ export default {
     vsColor:{
       type:String,
       default:'primary'
+    },
+    actionable:{
+      type:Boolean,
+      default: false
     }
   },
   methods:{
@@ -34,6 +39,13 @@ export default {
     border-radius 8px
     box-shadow: 0 20px 40px -8px rgba(0,0,0,0.1);
     margin 5px
+  .actionable
+    cursor pointer
+    transition-property box-shadow, transform
+    transition-duration .15s
+    &:hover
+      transform translateY(-2px)
+      box-shadow 0 18px 35px rgba(50, 50, 93, .1), 0 8px 15px rgba(0, 0, 0, .07)
 </style>
 
 <style lang="stylus"> // cannot be scoped due to inheritance
