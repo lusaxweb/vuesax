ca9a11f0cc5936d95a84077783e285d91e34331f
luis daniel <luisdanielrovira8@gmail.com>
2018-06-20 23:58:06 -0400

    'deploy'

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,20 @@ function resolve (dir) {
 }
 
 module.exports = {
+  productionSourceMap: false,
   configureWebpack: {
+    module: {
+      rules: [
+           {
+               test: /\.(woff2?|eot|ttf|otf)$/,
+               loader: 'file-loader',
+               options: {
+                   limit: 10000,
+                   name: '[name].[hash:7].[ext]'
+               }
+            }
+        ]
+    },
     plugins: [
       new CopyWebpackPlugin([
         {
