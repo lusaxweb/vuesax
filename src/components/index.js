2528f1c27eec06020b7411c08d38969d3b7b3394
Kylart <Kylart.dev@gmail.com>
2018-09-01 19:32:16 +0200

    Changed import into single line import export

--- src/components/index.js
+++ src/components/index.js
@@ -1,73 +1,35 @@
-import vsButton from './vsButton'
-import vsSelect from './vsSelect'
-import vsSwitch from './vsSwitch'
-import vsCheckbox from './vsCheckBox'
-import vsRadio from './vsRadio'
-import vsInput from './vsInput'
-import vsTabs from './vsTabs'
-import vsSlider from './vsSlider'
-import vsInputNumber from './vsInputNumber'
-import vsTooltip from './vsTooltip'
+export { default as vsButton } from './vsButton'
+export { default as vsSelect } from './vsSelect'
+export { default as vsSwitch } from './vsSwitch'
+export { default as vsCheckbox } from './vsCheckBox'
+export { default as vsRadio } from './vsRadio'
+export { default as vsInput } from './vsInput'
+export { default as vsTabs } from './vsTabs'
+export { default as vsSlider } from './vsSlider'
+export { default as vsInputNumber } from './vsInputNumber'
+export { default as vsTooltip } from './vsTooltip'
 
 // component not soport SSR
 
-import vsUpload from './vsUpload'
-import vsPopup from './vsPopup'
-import vsAlert from './vsAlert'
-import vsChip from './vsChip'
-// import vsChips from './vsChips'
-import vsProgress from './vsProgress'
-import vsCard from './vsCard'
-import vsTopbar from './vsTopbar'
-import vsList from './vsList'
-import vsAvatar from './vsAvatar'
-import vsPagination from './vsPagination'
-import vsBreadcrumb from './vsBreadcrumb'
-import vsSideBar from './vsSideBar'
-import vsPrompt from './vsPrompt'
-import vsDropDown from './vsDropDown'
-import vsDivider from './vsDivider'
-import vsCollapse from './vsCollapse'
+export { default as vsUpload } from './vsUpload'
+export { default as vsPopup } from './vsPopup'
+export { default as vsAlert } from './vsAlert'
+export { default as vsChip } from './vsChip'
+// export { default as vsChips } from './vsChips'
+export { default as vsProgress } from './vsProgress'
+export { default as vsCard } from './vsCard'
+export { default as vsTopbar } from './vsTopbar'
+export { default as vsList } from './vsList'
+export { default as vsAvatar } from './vsAvatar'
+export { default as vsPagination } from './vsPagination'
+export { default as vsBreadcrumb } from './vsBreadcrumb'
+export { default as vsSideBar } from './vsSideBar'
+export { default as vsPrompt } from './vsPrompt'
+export { default as vsDropDown } from './vsDropDown'
+export { default as vsDivider } from './vsDivider'
+export { default as vsCollapse } from './vsCollapse'
 //New Component import
 
-
 //layout
-import vsRow from '../layout/vsRow/'
-import vsCol from '../layout/vsCol/'
-
-
-
-export {
-  vsButton,
-  vsSelect,
-  vsSwitch,
-  vsCheckbox,
-  vsRadio,
-  vsInput,
-  vsTabs,
-  vsSlider,
-  vsInputNumber,
-  vsUpload,
-  vsPopup,
-  vsAlert,
-  vsChip,
-  // vsChips,
-  vsProgress,
-  vsCard,
-  vsTopbar,
-  vsList,
-  vsRow,
-  vsCol,
-  vsAvatar,
-  vsPagination,
-  vsBreadcrumb,
-  vsSideBar,
-  vsPrompt,
-  vsDropDown,
-  vsDivider,
-  vsCollapse,
-  vsTooltip,
-  // New Component export
-
-
-}
+export { default as vsRow } from '../layout/vsRow/'
+export { default as vsCol } from '../layout/vsCol/'
