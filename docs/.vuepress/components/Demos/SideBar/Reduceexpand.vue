2c3586f0f26db72b4fd0f391c9b9c4c6d56c4b4e
Bruno Henry <brunohenryt@hotmail.com>
2018-09-18 02:33:36 -0300

    added button on sidebar for toggle reduce

--- docs/.vuepress/components/Demos/SideBar/Reduceexpand.vue
+++ docs/.vuepress/components/Demos/SideBar/Reduceexpand.vue
@@ -4,14 +4,14 @@
 
     <vs-button @click="active=!active, notExpand = false" vs-color="success" vs-type="filled">Open Sidebar Reduce-expand</vs-button>
     <vs-button @click="active=!active, notExpand = true" vs-color="success" vs-type="filled">Open Sidebar Reduce-only</vs-button>
-    <vs-sidebar reduce :reduce-not-hover-expand="notExpand" parent="body" default-index="1"  color="success" class="sidebarx" spacer v-model="active">
+    <vs-sidebar :reduce="reduce" :reduce-not-hover-expand="notExpand" parent="body" default-index="1"  color="success" class="sidebarx" spacer v-model="active">
 
       <div class="header-sidebar" slot="header">
         <vs-avatar  size="70px" src="https://randomuser.me/api/portraits/men/85.jpg"/>
       </div>
-      <vs-sidebar-group open title="Aplication">
-        <vs-sidebar-item index="1" icon="question_answer">
-          Dashboard
+      <vs-sidebar-group open title="Application">
+        <vs-sidebar-item index="1" icon="menu" @click="reduce=!reduce">
+         Toggle Sidebar
         </vs-sidebar-item>
         <vs-sidebar-item index="5" icon="verified_user">
           Configurations
@@ -31,7 +31,7 @@
           History
         </vs-sidebar-item>
         <vs-sidebar-item index="3" icon="https">
-          security
+          Security
         </vs-sidebar-item>
         <vs-sidebar-item index="4" icon="help">
           Help
@@ -45,7 +45,7 @@
 
 
       <vs-sidebar-item index="6" icon="account_box">
-        Perfile
+        Profile
       </vs-sidebar-item>
 
       <div class="footer-sidebar" slot="footer">
@@ -61,7 +61,8 @@
 export default {
   data:()=>({
     active:false,
-    notExpand: false
+    notExpand: false,
+    reduce: true
   })
 }
 </script>
