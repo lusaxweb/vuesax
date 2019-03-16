6691b26cf8ea7983b67329e87704723495641a67
ldrovira <luisdanielrovira8@gmail.com>
2019-03-16 08:31:17 -0400

    'deploy'

--- src/components/vsCheckBox/vsCheckBox.vue
+++ src/components/vsCheckBox/vsCheckBox.vue
@@ -69,9 +69,9 @@ export default {
     listeners(){
       return {
         ...this.$listeners,
-        change: (evt) => {
-          this.toggleValue(evt)
-        },
+        // change: (evt) => {
+        //   this.toggleValue(evt)
+        // },
         input: (evt) => {
           this.toggleValue(evt)
         }
