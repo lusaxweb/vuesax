c7f941da903f242b9d843d0f628d128ecb8ee72e
Mathias Eudeline <m.eudeline@crown.fr>
2019-06-05 16:38:25 +0200

    Adding vs-name property from vsRadio to the docs

--- docs/.vuepress/components/Demos/Radio/Default.vue
+++ docs/.vuepress/components/Demos/Radio/Default.vue
@@ -4,13 +4,13 @@
       {{radios1}}
     </li>
     <li>
-      <vs-radio v-model="radios1" vs-value="luis">Luis</vs-radio>
+      <vs-radio v-model="radios1" vs-name="radio1" vs-value="luis">Luis</vs-radio>
     </li>
     <li>
-      <vs-radio v-model="radios1" vs-value="carols">Carols</vs-radio>
+      <vs-radio v-model="radios1" vs-name="radio1" vs-value="carols">Carols</vs-radio>
     </li>
     <li>
-      <vs-radio v-model="radios1" vs-value="summer">summer</vs-radio>
+      <vs-radio v-model="radios1" vs-name="radio1" vs-value="summer">summer</vs-radio>
     </li>
     <li>
       <vs-radio disabled="true" v-model="radios1" vs-value="lyon">Lyon - disabled</vs-radio>
