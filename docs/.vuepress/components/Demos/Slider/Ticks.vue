17412cb43cd353dcda2f2621d7b1d69d94958070
Luis Pedraz <LPedraz1981@gmail.com>
2019-07-02 10:55:41 +0200

    New sample with a decimal slidebar

--- docs/.vuepress/components/Demos/Slider/Ticks.vue
+++ docs/.vuepress/components/Demos/Slider/Ticks.vue
@@ -2,18 +2,21 @@
   <div class="">
     <vs-slider step=10 v-model="value1"/>
     <vs-slider ticks step=25 v-model="value2"/>
+    <vs-slider ticks step=0.5 :step-decimals="stepDecimals" v-model="value3"/>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      value1:60,
-      value2:25
-    }
+      value1: 60,
+      value2: 25,
+      value3: 25.5,
+      stepDecimals: true
+    };
   }
-}
+};
 </script>
 
 <style lang="css">
