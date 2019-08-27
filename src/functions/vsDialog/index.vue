ce8b0c970682ba06fd4d478929ed0fd2cec9b754
JD <jdsolanki0001@gmail.com>
2019-08-27 17:22:57 +0530

    fix: dialog component outside click console error

--- src/functions/vsDialog/index.vue
+++ src/functions/vsDialog/index.vue
@@ -186,7 +186,7 @@ export default {
     handleClose(event,con){
       if(con){
         if(event.target.className.indexOf('vs-dialog-dark')!=-1 && this.type == 'alert'){
-          this.active = false
+          this.factive = false
           this.$emit('update:active',false)
         } else if (event.target.className.indexOf('vs-dialog-dark')!=-1) {
           this.rebound()
