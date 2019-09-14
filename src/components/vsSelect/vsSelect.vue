7a17a7998a3bba729b12a9067301ddc843de7208
Tiago Simões <tiago.simoes@msn.com>
2019-09-14 19:01:04 +0100

    fix v-select clicking outside when it was opened

--- src/components/vsSelect/vsSelect.vue
+++ src/components/vsSelect/vsSelect.vue
@@ -6,8 +6,8 @@
       'input-select-validate-success':success,
       'input-select-validate-danger':danger,
       'input-select-validate-warning':warning}"
-    class="con-select"
-    :style="getWidth">
+    :style="getWidth"
+    class="con-select">
     <label
       v-if="label"
       ref="inputSelectLabel"
@@ -27,8 +27,8 @@
 
       <button
         :class="{'activeBtnClear': activeBtnClear}"
-        @click="clearValue"
-        class="icon-select-clear vs-select--icon-clear">
+        class="icon-select-clear vs-select--icon-clear"
+        @click="clearValue">
         <i class="material-icons">
           close
         </i>
@@ -111,397 +111,408 @@
 </template>
 
 <script>
-import utils from '../../utils'
+import utils from "../../utils";
 export default {
-  name:'VsSelect',
-  props:{
-    value:{},
-    noData:{
-      default:'data no available',
-      type:String
+  name: "VsSelect",
+  props: {
+    value: {},
+    noData: {
+      default: "data no available",
+      type: String
     },
-    maxSelected:{
-      default:null,
-      type:[Number,String]
-    },
-    autocomplete:{
-      default:false,
-      type:Boolean
-    },
-    color:{
-      default:'primary',
-      type:String
-    },
-    multiple:{
-      default:false,
-      type:Boolean
-    },
-    label:{
-      default:null,
-      type:[String]
-    },
-    success:{
-      default:false,
-      type:Boolean
-    },
-    danger:{
-      default:false,
-      type:Boolean
-    },
-    warning:{
-      default:false,
-      type:Boolean
-    },
-    successText:{
+    maxSelected: {
       default: null,
-      type:String
+      type: [Number, String]
     },
-    dangerText:{
+    autocomplete: {
+      default: false,
+      type: Boolean
+    },
+    color: {
+      default: "primary",
+      type: String
+    },
+    multiple: {
+      default: false,
+      type: Boolean
+    },
+    label: {
       default: null,
-      type:String
+      type: [String]
     },
-    warningText:{
+    success: {
+      default: false,
+      type: Boolean
+    },
+    danger: {
+      default: false,
+      type: Boolean
+    },
+    warning: {
+      default: false,
+      type: Boolean
+    },
+    successText: {
       default: null,
-      type:String
+      type: String
     },
-    descriptionText:{
+    dangerText: {
       default: null,
-      type:String
+      type: String
     },
-    iconPack:{
-      default: 'material-icons',
-      type:String
-    },
-    icon:{
-      default: 'keyboard_arrow_down',
-      type:String
-    },
-    iconClear:{
-      default: 'close',
-      type:String
-    },
-    width:{
+    warningText: {
       default: null,
-      type: String,
+      type: String
+    },
+    descriptionText: {
+      default: null,
+      type: String
+    },
+    iconPack: {
+      default: "material-icons",
+      type: String
+    },
+    icon: {
+      default: "keyboard_arrow_down",
+      type: String
+    },
+    iconClear: {
+      default: "close",
+      type: String
+    },
+    width: {
+      default: null,
+      type: String
     }
   },
-  data:()=>({
-    valueFilter:'',
-    active:false,
-    valuex:'',
-    clear:false,
-    scrollx:false,
-    cords:{},
-    filterx:false
+  data: () => ({
+    valueFilter: "",
+    active: false,
+    valuex: "",
+    clear: false,
+    scrollx: false,
+    cords: {},
+    filterx: false
   }),
-  computed:{
+  computed: {
     activeBtnClear() {
-      return this.autocomplete && this.filterx
+      return this.autocomplete && this.filterx;
     },
     getWidth() {
-      return this.width ? `width:${this.width};` : null
+      return this.width ? `width:${this.width};` : null;
     },
     parent() {
-      return this
+      return this;
     },
-    listeners(){
+    listeners() {
       return {
         ...this.$listeners,
-        blur: (event) => {
-          if(this.autocomplete && event.relatedTarget?!event.relatedTarget.closest('.vs-select--options'):false ){
-            this.closeOptions()
+        blur: event => {
+          if (
+            this.autocomplete && event.relatedTarget
+              ? !event.relatedTarget.closest(".vs-select--options")
+              : false
+          ) {
+            this.closeOptions();
           }
-          this.$emit('blur',event)
+          this.$emit("blur", event);
         },
-        focus: (event) => {
-          this.$emit('focus',event)
+        focus: event => {
+          this.$emit("focus", event);
           //document.removeEventListener('click',this.clickBlur)
-          this.focus(event)
+          this.focus(event);
         },
-        input: (event) => {
+        input: event => {
           if (this.autocomplete) {
-            this.$emit('input-change', event)
+            this.$emit("input-change", event);
           }
         },
-        keyup: (event) => {
-          if(event.key == 'ArrowDown' || event.key == 'ArrowUp'){
-            event.preventDefault()
-            let childrens = this.$children.filter((item)=>{
-              return item.visible
-            })
-            childrens[0].$el.querySelector('.vs-select--item').focus()
+        keyup: event => {
+          if (event.key == "ArrowDown" || event.key == "ArrowUp") {
+            event.preventDefault();
+            let childrens = this.$children.filter(item => {
+              return item.visible;
+            });
+            childrens[0].$el.querySelector(".vs-select--item").focus();
           } else {
-            if(this.autocomplete){
-              this.filterItems(event.target.value)
+            if (this.autocomplete) {
+              this.filterItems(event.target.value);
             }
           }
 
-          this.$children.map((item)=>{
-            item.valueInputx = this.$refs.inputselect.value
-          })
+          this.$children.map(item => {
+            item.valueInputx = this.$refs.inputselect.value;
+          });
         }
-      }
-    },
+      };
+    }
   },
-  watch:{
-    value(event){
+  watch: {
+    value(event) {
       this.valuex = this.value;
-      this.$emit('change',event)
+      this.$emit("change", event);
     },
-    active(){
+    active() {
       this.$nextTick(() => {
-        if(this.active){
-          utils.insertBody(this.$refs.vsSelectOptions)
-          setTimeout( () => {
-            this.$children.forEach((item)=>{
+        if (this.active) {
+          utils.insertBody(this.$refs.vsSelectOptions);
+          setTimeout(() => {
+            this.$children.forEach(item => {
               if (item.focusValue) {
-                item.focusValue()
+                item.focusValue();
               }
-            })
-            if(this.$refs.ulx.scrollHeight >= 260) this.scrollx = true
+            });
+            if (this.$refs.ulx.scrollHeight >= 260) this.scrollx = true;
           }, 100);
         } else {
-          let [parent] = document.getElementsByTagName('body')
-          parent.removeChild(this.$refs.vsSelectOptions)
+          let [parent] = document.getElementsByTagName("body");
+          parent.removeChild(this.$refs.vsSelectOptions);
         }
-      })
-    },
+      });
+    }
   },
-  mounted(){
+  mounted() {
     // this.$refs.inputselect.value = this.value
-    this.changeValue()
+    this.changeValue();
     if (this.active) {
-      utils.insertBody(this.$refs.vsSelectOptions)
+      utils.insertBody(this.$refs.vsSelectOptions);
     }
   },
   beforeDestroy() {
-    let [parent] = document.getElementsByTagName('body')
+    let [parent] = document.getElementsByTagName("body");
 
-    if (parent &&
-        this.$refs.vsSelectOptions &&
-        this.$refs.vsSelectOptions.parentNode === parent) {
-      parent.removeChild(this.$refs.vsSelectOptions)
+    if (
+      parent &&
+      this.$refs.vsSelectOptions &&
+      this.$refs.vsSelectOptions.parentNode === parent
+    ) {
+      parent.removeChild(this.$refs.vsSelectOptions);
     }
   },
-  updated(){
-    if(!this.active){
-      this.changeValue()
+  updated() {
+    if (!this.active) {
+      this.changeValue();
     }
   },
-  methods:{
+  methods: {
     clearValue() {
-      this.focus()
-      this.filterItems('')
-      this.changeValue()
+      this.focus();
+      this.filterItems("");
+      this.changeValue();
     },
-    addMultiple(value){
+    addMultiple(value) {
       let currentValues = this.value ? this.value : [];
-      if(currentValues.includes(value)){
-        currentValues.splice(currentValues.indexOf(value),1)
-        this.$emit('input', currentValues);
-        this.changeValue()
-        if(this.autocomplete) {
-          this.$refs.inputselect.focus()
+      if (currentValues.includes(value)) {
+        currentValues.splice(currentValues.indexOf(value), 1);
+        this.$emit("input", currentValues);
+        this.changeValue();
+        if (this.autocomplete) {
+          this.$refs.inputselect.focus();
         }
       } else {
-        if(this.autocomplete){
-          currentValues.push(value)
-          this.$emit('input', currentValues);
-          this.filterItems('')
-          this.changeValue()
+        if (this.autocomplete) {
+          currentValues.push(value);
+          this.$emit("input", currentValues);
+          this.filterItems("");
+          this.changeValue();
           // this.$refs.inputselect.value += ','
-          this.$refs.inputselect.focus()
+          this.$refs.inputselect.focus();
         } else {
-          currentValues.push(value)
-          this.$emit('input', currentValues);
-          this.changeValue()
+          currentValues.push(value);
+          this.$emit("input", currentValues);
+          this.changeValue();
         }
       }
-
     },
-    filterItems(value){
-      if(value){
-        this.filterx = true
+    filterItems(value) {
+      if (value) {
+        this.filterx = true;
       } else {
-        this.filterx = false
+        this.filterx = false;
       }
-      let items = this.$children
+      let items = this.$children;
 
-      items.forEach((item)=>{
-        if(item.$children.length > 0) {
-          items = [...items,...item.$children]
+      items.forEach(item => {
+        if (item.$children.length > 0) {
+          items = [...items, ...item.$children];
         }
-      })
+      });
 
-      items.map((item)=>{
+      items.map(item => {
+        if (!("text" in item)) return;
 
-        if (!('text' in item)) return
+        let text = item.text;
 
-        let text = item.text
-
-        if(this.multiple){
-          let valuesx = value.split(',')
-          valuesx.forEach((value_multi)=>{
-            if(text.toUpperCase().indexOf(value_multi.toUpperCase()) == -1){
-              item.visible = false
+        if (this.multiple) {
+          let valuesx = value.split(",");
+          valuesx.forEach(value_multi => {
+            if (text.toUpperCase().indexOf(value_multi.toUpperCase()) == -1) {
+              item.visible = false;
             } else {
-              item.visible = true
+              item.visible = true;
             }
-          })
-
+          });
         } else {
-          if(text.toUpperCase().indexOf(value.toUpperCase()) == -1){
-            item.visible = false
+          if (text.toUpperCase().indexOf(value.toUpperCase()) == -1) {
+            item.visible = false;
           } else {
-            item.visible = true
+            item.visible = true;
           }
         }
+      });
 
-      })
+      let lengthx = items.filter(item => {
+        return item.visible;
+      });
 
-      let lengthx = items.filter((item)=>{
-        return item.visible
-      })
-
-      if(lengthx.length == 0){
-        this.clear = true
+      if (lengthx.length == 0) {
+        this.clear = true;
       } else {
-        this.clear = false
+        this.clear = false;
       }
 
-      this.$nextTick(()=>{
-        this.cords = this.changePosition()
-      })
+      this.$nextTick(() => {
+        this.cords = this.changePosition();
+      });
     },
-    changeValue(){
-      this.filterx = false
-      if(this.multiple){
+    changeValue() {
+      this.filterx = false;
+      if (this.multiple) {
         let values = this.value ? this.value : [];
-        let options = this.$children
+        let options = this.$children;
 
-        options.forEach((item)=>{
-          if(item.$children.length > 0) {
-            options = [...options,...item.$children]
+        options.forEach(item => {
+          if (item.$children.length > 0) {
+            options = [...options, ...item.$children];
           }
-        })
+        });
 
-        let optionsValues = []
-        values.forEach((item)=>{
-          options.forEach((item_option)=>{
-            if(item_option.value == item) {
-              let text = item_option.text
-              text = text.replace('check_circle','')
-              optionsValues.push(text.trim())
+        let optionsValues = [];
+        values.forEach(item => {
+          options.forEach(item_option => {
+            if (item_option.value == item) {
+              let text = item_option.text;
+              text = text.replace("check_circle", "");
+              optionsValues.push(text.trim());
             }
-          })
-        })
-        this.$refs.inputselect.value = optionsValues.toString()
+          });
+        });
+        this.$refs.inputselect.value = optionsValues.toString();
       } else {
-        if(this.$refs.inputselect) {
-          this.$refs.inputselect.value = this.valuex
+        if (this.$refs.inputselect) {
+          this.$refs.inputselect.value = this.valuex;
         }
       }
     },
-    focus(){
-      this.active = true
-      this.setLabelClass(this.$refs.inputSelectLabel, true)
-      let inputx = this.$refs.inputselect
-      // setTimeout( ()=> {
-      //   document.addEventListener('click',this.clickBlur)
-      // }, 100);
-      if(this.autocomplete && this.multiple){
-        setTimeout( ()=> {
-          if(inputx.value){
-            this.$refs.inputselect.value = inputx.value += ','
+    focus() {
+      this.active = true;
+      this.setLabelClass(this.$refs.inputSelectLabel, true);
+      let inputx = this.$refs.inputselect;
+      setTimeout(() => {
+        document.addEventListener("click", this.clickBlur);
+      }, 100);
+      if (this.autocomplete && this.multiple) {
+        setTimeout(() => {
+          if (inputx.value) {
+            this.$refs.inputselect.value = inputx.value += ",";
           }
           inputx.selectionStart = inputx.selectionEnd = 10000;
         }, 10);
-
       } else if (this.autocomplete && !this.multiple) {
-        this.$refs.inputselect.select()
+        this.$refs.inputselect.select();
       }
 
       if (!this.autocomplete) {
-        if(this.multiple?this.value.length == 0:!this.value || this.multiple){
-          setTimeout( () => {
-            const el = this.$children[0].$el.querySelector('.vs-select--item')
-            if (el) el.focus()
+        if (
+          this.multiple ? this.value.length == 0 : !this.value || this.multiple
+        ) {
+          setTimeout(() => {
+            const el = this.$children[0].$el.querySelector(".vs-select--item");
+            if (el) el.focus();
           }, 50);
         }
       }
-      this.$nextTick(()=>{
-        this.cords = this.changePosition()
-      })
-
+      this.$nextTick(() => {
+        this.cords = this.changePosition();
+      });
     },
-    clickBlur(event){
-      let closestx = event.target.closest('.vs-select--options')
+    clickBlur(event) {
+      let closestx = event.target.closest(".vs-select--options");
 
-      if(!closestx){
-        this.closeOptions()
-        if(this.autocomplete){
-          this.filterItems('')
+      if (!closestx) {
+        this.closeOptions();
+        if (this.autocomplete) {
+          this.filterItems("");
         }
-        this.changeValue()
+        this.changeValue();
       }
     },
-    closeOptions(){
+    closeOptions() {
       // this.$refs.inputselect.blur()
-      this.active = false
-      this.setLabelClass(this.$refs.inputSelectLabel, false)
-      document.removeEventListener('click',this.clickBlur)
+      this.active = false;
+      this.setLabelClass(this.$refs.inputSelectLabel, false);
+      document.removeEventListener("click", this.clickBlur);
     },
-    changePosition(){
-      let elx = this.$refs.inputselect
-      let content = this.$refs.vsSelectOptions
-      let conditional = this.autocomplete
-      let topx = 0
-      let leftx = 0
-      let widthx = 0
+    changePosition() {
+      let elx = this.$refs.inputselect;
+      let content = this.$refs.vsSelectOptions;
+      let conditional = this.autocomplete;
+      let topx = 0;
+      let leftx = 0;
+      let widthx = 0;
       let scrollTopx = window.pageYOffset || document.documentElement.scrollTop;
-      if(elx.getBoundingClientRect().top + content.scrollHeight + 20 >= window.innerHeight) {
-        topx = (elx.getBoundingClientRect().top + elx.clientHeight) + scrollTopx - content.scrollHeight
-        if(conditional){
-          topx = topx - elx.clientHeight - 5
+      if (
+        elx.getBoundingClientRect().top + content.scrollHeight + 20 >=
+        window.innerHeight
+      ) {
+        topx =
+          elx.getBoundingClientRect().top +
+          elx.clientHeight +
+          scrollTopx -
+          content.scrollHeight;
+        if (conditional) {
+          topx = topx - elx.clientHeight - 5;
         }
       } else {
-        topx = conditional?(elx.getBoundingClientRect().top + elx.clientHeight) + scrollTopx + 5:elx.getBoundingClientRect().top + scrollTopx
+        topx = conditional
+          ? elx.getBoundingClientRect().top + elx.clientHeight + scrollTopx + 5
+          : elx.getBoundingClientRect().top + scrollTopx;
       }
 
-      leftx = elx.getBoundingClientRect().left
-      widthx = elx.offsetWidth
+      leftx = elx.getBoundingClientRect().left;
+      widthx = elx.offsetWidth;
 
       let cords = {
         left: `${leftx}px`,
         top: `${topx}px`,
         width: `${widthx}px`
-      }
+      };
 
-      return cords
+      return cords;
     },
     beforeEnter(el) {
-      el.style.height = 0
+      el.style.height = 0;
     },
-    enter(el, done){
-      let h = el.scrollHeight
-      el.style.height = h + 'px'
-      done()
+    enter(el, done) {
+      let h = el.scrollHeight;
+      el.style.height = h + "px";
+      done();
     },
-    leave: function (el) {
-      el.style.height = 0 + 'px'
+    leave: function(el) {
+      el.style.height = 0 + "px";
     },
     setLabelClass: function(label, focusing) {
       if (!label) {
-        return
+        return;
       }
 
       if (focusing) {
-        label.classList.add('input-select-label-' + this.color + '--active')
-        return
+        label.classList.add("input-select-label-" + this.color + "--active");
+        return;
       }
 
-      label.classList.remove('input-select-label-' + this.color + '--active')
+      label.classList.remove("input-select-label-" + this.color + "--active");
     }
   }
-}
+};
 </script>
