37dff5b124bc8a2224af565de5916850fa791080
Tiago Simões <tiago.simoes@msn.com>
2019-09-27 23:46:07 +0100

    added missing props for table description strings

--- src/components/vsTable/vsTable.vue
+++ src/components/vsTable/vsTable.vue
@@ -1,43 +1,37 @@
 <template>
   <div
     :class="[{'stripe': stripe, 'hoverFlat': hoverFlat}, `vs-table-${color}`]"
-    class="vs-component vs-con-table">
+    class="vs-component vs-con-table"
+  >
     <!-- header -->
     <header class="header-table vs-table--header">
       <slot name="header"></slot>
-      <div
-        v-if="search"
-        class="con-input-search vs-table--search">
-        <input
-          v-model="searchx"
-          class="input-search vs-table--search-input"
-          type="text">
+      <div v-if="search" 
+class="con-input-search vs-table--search">
+        <input v-model="searchx" 
+class="input-search vs-table--search-input" type="text" />
         <vs-icon icon="search"></vs-icon>
       </div>
     </header>
     <div class="con-tablex vs-table--content">
-      <div
-        :style="styleConTbody"
-        class="vs-con-tbody vs-table--tbody ">
-        <table
-          ref="table"
-          class="vs-table vs-table--tbody-table">
-          <thead
-            ref="thead"
-            class="vs-table--thead">
+      <div :style="styleConTbody" 
+class="vs-con-tbody vs-table--tbody">
+        <table ref="table" 
+class="vs-table vs-table--tbody-table">
+          <thead ref="thead" 
+class="vs-table--thead">
             <tr>
-              <th
-                v-if="multiple || hasExpadableData"
-                class="td-check">
-                <span
-                  v-if="multiple"
-                  class="con-td-check">
+              <th v-if="multiple || hasExpadableData" 
+class="td-check">
+                <span v-if="multiple" 
+class="con-td-check">
                   <vs-checkbox
                     :key="isCheckedLine ? 'remove' : 'check'"
                     :icon="isCheckedLine ? 'remove' : 'check'"
                     :checked="isCheckedMultiple"
                     size="small"
-                    @click="changeCheckedMultiple"/>
+                    @click="changeCheckedMultiple"
+                  />
                 </span>
               </th>
               <slot name="thead"></slot>
@@ -46,23 +40,22 @@
           <slot :data="datax"></slot>
         </table>
       </div>
-      <div
-        v-if="isNoData"
-        class="not-data-table vs-table--not-data">
-        {{ noDataText }}
-      </div>
+      <div v-if="isNoData" 
+class="not-data-table vs-table--not-data">{{ noDataText }}</div>
 
-      <div
-        v-if="pagination"
-        class="con-pagination-table vs-table--pagination">
+      <div v-if="pagination" 
+class="con-pagination-table vs-table--pagination">
         <vs-pagination
           :total="searchx && !sst ? getTotalPagesSearch : getTotalPages"
-          :sizeArray="data.length"
-          :maxItems="maxItemsx"
+          :size-array="data.length"
+          :max-items="maxItemsx"
           :description="description"
-          :descriptionItems="descriptionItems"
+          :description-items="descriptionItems"
+          :description-title="descriptionTitle"
+          :description-body="descriptionBody"
+          v-model="currentx"
           @changeMaxItems="changeMaxItems"
-          v-model="currentx"></vs-pagination>
+        ></vs-pagination>
       </div>
     </div>
   </div>
@@ -71,56 +64,68 @@
 <script>
 export default {
   name: "VsTable",
-  props:{
-    value:{},
+  props: {
+    value: {},
     color: {
-      default:'primary',
+      default: "primary",
       type: String
+    },
+    descriptionTitle: {
+      type: String,
+      default: "qwd22"
+    },
+    descriptionConnector: {
+      type: String,
+      default: "of"
+    },
+    descriptionBody: {
+      type: String,
+      default: "Pages"
     },
     noDataText: {
-      default: 'No data Available',
+      default: "No data Available",
       type: String
     },
-    stripe:{
+    stripe: {
       default: false,
       type: Boolean
     },
-    hoverFlat:{
+    hoverFlat: {
       default: false,
       type: Boolean
     },
-    maxHeight:{
-      default: 'auto',
+    maxHeight: {
+      default: "auto",
       type: String
     },
-    multiple:{
+    multiple: {
       default: false,
       type: Boolean
     },
-    data:{
-      default: null,
+    data: {
+      default: null
     },
-    notSpacer:{
-      default:false,
-      type:Boolean
-    },
-    search:{
+    notSpacer: {
       default: false,
       type: Boolean
     },
-    maxItems:{
+    search: {
+      default: false,
+      type: Boolean
+    },
+    maxItems: {
       default: 5,
       type: [Number, String]
     },
-    pagination:{
+    pagination: {
       default: false,
       type: Boolean
     },
-    description:{
+    description: {
       default: false,
       type: Boolean
     },
-    descriptionItems:{
+    descriptionItems: {
       default: () => [],
       type: Array
     },
@@ -128,7 +133,7 @@ export default {
       default: 1,
       type: Number | String
     },
-    sst:{
+    sst: {
       default: false,
       type: Boolean
     },
@@ -137,99 +142,103 @@ export default {
       default: 0
     }
   },
-  data:()=>({
-    headerWidth: '100%',
+  data: () => ({
+    headerWidth: "100%",
     trs: [],
     datax: [],
     searchx: null,
     currentx: 1,
     maxItemsx: 5,
-    hasExpadableData: false,
+    hasExpadableData: false
   }),
-  computed:{
+  computed: {
     getTotalPages() {
-      return Math.ceil(this.data.length / this.maxItemsx)
+      return Math.ceil(this.data.length / this.maxItemsx);
     },
     getTotalPagesSearch() {
-      let dataBase = this.data
+      let dataBase = this.data;
 
-      let filterx = dataBase.filter((tr)=>{
-        let values = this.getValues(tr).toString().toLowerCase()
-        return values.indexOf(this.searchx.toLowerCase()) != -1
-      })
+      let filterx = dataBase.filter(tr => {
+        let values = this.getValues(tr)
+          .toString()
+          .toLowerCase();
+        return values.indexOf(this.searchx.toLowerCase()) != -1;
+      });
 
-      return Math.ceil(filterx.length / this.maxItemsx)
+      return Math.ceil(filterx.length / this.maxItemsx);
     },
     isNoData() {
-      if(typeof(this.datax) == Object) {
-        return this.datax? Object.keys(this.datax).length == 0:false && this.search
+      if (typeof this.datax == Object) {
+        return this.datax
+          ? Object.keys(this.datax).length == 0
+          : false && this.search;
       } else {
-        return this.datax?this.datax.length == 0:false && this.search
+        return this.datax ? this.datax.length == 0 : false && this.search;
       }
     },
-    isCheckedLine () {
-      let lengthx = this.data.length
-      let lengthSelected = this.value.length
-      return lengthx !== lengthSelected
+    isCheckedLine() {
+      let lengthx = this.data.length;
+      let lengthSelected = this.value.length;
+      return lengthx !== lengthSelected;
     },
-    isCheckedMultiple () {
-      return this.value.length > 0
+    isCheckedMultiple() {
+      return this.value.length > 0;
     },
-    styleConTbody () {
+    styleConTbody() {
       return {
         maxHeight: this.maxHeight,
-        overflow: this.maxHeight != 'auto'?'auto':null
-      }
+        overflow: this.maxHeight != "auto" ? "auto" : null
+      };
     },
-    getThs () {
-      let ths = this.$slots.thead.filter(item => item.tag )
-      return ths.length
+    getThs() {
+      let ths = this.$slots.thead.filter(item => item.tag);
+      return ths.length;
     },
-    tableHeaderStyle () {
+    tableHeaderStyle() {
       return {
         width: this.headerWidth
-      }
-    },
+      };
+    }
   },
-  watch:{
+  watch: {
     currentPage() {
-      this.currentx = this.currentPage
+      this.currentx = this.currentPage;
     },
     currentx() {
-      if(this.sst) {
-        this.$emit('change-page', this.currentx)
+      if (this.sst) {
+        this.$emit("change-page", this.currentx);
       } else {
-        this.loadData()
+        this.loadData();
       }
     },
     maxItems(val) {
-      this.maxItemsx = val
-      this.loadData()
+      this.maxItemsx = val;
+      this.loadData();
     },
     maxItemsx() {
-      this.loadData()
+      this.loadData();
     },
     data() {
-      this.loadData()
+      this.loadData();
       this.$nextTick(() => {
-        if(this.datax.length > 0) {
-          this.changeTdsWidth()
+        if (this.datax.length > 0) {
+          this.changeTdsWidth();
         }
-      })
+      });
     },
     searchx() {
-      if(this.sst) {
-        this.$emit('search', this.searchx)
+      if (this.sst) {
+        this.$emit("search", this.searchx);
       } else {
-        this.loadData()
-        this.currentx = 1
+        this.loadData();
+        this.currentx = 1;
       }
     }
   },
-  mounted () {
-    window.addEventListener('resize', this.listenerChangeWidth)
-    this.maxItemsx = this.maxItems
-    this.loadData()
+  mounted() {
+    window.addEventListener("resize", this.listenerChangeWidth);
+    this.maxItemsx = this.maxItems;
+    this.loadData();
 
     // this.$nextTick(() => {
     //   if(this.datax.length > 0) {
@@ -237,169 +246,166 @@ export default {
     //   }
     // })
   },
-  destroyed () {
-    window.removeEventListener('resize', this.listenerChangeWidth)
+  destroyed() {
+    window.removeEventListener("resize", this.listenerChangeWidth);
   },
-  methods:{
+  methods: {
     loadData() {
-      let max = Math.ceil(this.currentx * this.maxItemsx)
-      let min = max - this.maxItemsx
+      let max = Math.ceil(this.currentx * this.maxItemsx);
+      let min = max - this.maxItemsx;
 
-      if(!this.searchx || this.sst) {
-        this.datax = this.pagination ? this.getItems(min, max) : this.data || [];
+      if (!this.searchx || this.sst) {
+        this.datax = this.pagination
+          ? this.getItems(min, max)
+          : this.data || [];
       } else {
-        this.datax = this.pagination ? this.getItemsSearch(true ,min, max) : this.getItemsSearch(false ,min, max) || []
+        this.datax = this.pagination
+          ? this.getItemsSearch(true, min, max)
+          : this.getItemsSearch(false, min, max) || [];
       }
     },
     getItems(min, max) {
-      let items = []
+      let items = [];
       this.data.forEach((item, index) => {
-        if(index >= min && index < max) {
-          items.push(item)
+        if (index >= min && index < max) {
+          items.push(item);
         }
-      })
-      return items
+      });
+      return items;
     },
-    getItemsSearch(pagination = false,min, max) {
-      let dataBase = this.data
+    getItemsSearch(pagination = false, min, max) {
+      let dataBase = this.data;
 
-      let filterx = dataBase.filter((tr)=>{
-        let values = this.getValues(tr).toString().toLowerCase()
-        return values.indexOf(this.searchx.toLowerCase()) != -1
-      })
+      let filterx = dataBase.filter(tr => {
+        let values = this.getValues(tr)
+          .toString()
+          .toLowerCase();
+        return values.indexOf(this.searchx.toLowerCase()) != -1;
+      });
 
-      let items = []
+      let items = [];
 
       filterx.forEach((item, index) => {
-        if(index >= min && index < max) {
-          items.push(item)
+        if (index >= min && index < max) {
+          items.push(item);
         }
-      })
+      });
 
-      return items
+      return items;
     },
     sort(key, active) {
-      if(this.sst) {
-        this.$emit('sort', key, active)
-        return
+      if (this.sst) {
+        this.$emit("sort", key, active);
+        return;
       }
-      let datax = (this.pagination) ? this.data : this.datax
+      let datax = this.pagination ? this.data : this.datax;
 
-      function compare(a,b) {
-        if (a[key] < b[key])
-          return active?1:-1
-        if (a[key] > b[key])
-          return active?-1:1;
+      function compare(a, b) {
+        if (a[key] < b[key]) return active ? 1 : -1;
+        if (a[key] > b[key]) return active ? -1 : 1;
         return 0;
       }
 
-      this.datax = datax.sort(compare)
+      this.datax = datax.sort(compare);
     },
     getValues(obj) {
-      let valuesx = Object.values(obj)
-      let strings = []
+      let valuesx = Object.values(obj);
+      let strings = [];
 
-      function getStrings (obj) {
-
-        if(Array.isArray(obj)) {
-
-          strings = [...strings,...obj]
-          obj.forEach((item) => {
-            getStrings(item)
-          })
-
-        } else if (typeof obj == 'object' && obj != null) {
-          let subObj = Object.values(obj)
-          strings = [...strings,...subObj]
-          getStrings(subObj)
+      function getStrings(obj) {
+        if (Array.isArray(obj)) {
+          strings = [...strings, ...obj];
+          obj.forEach(item => {
+            getStrings(item);
+          });
+        } else if (typeof obj == "object" && obj != null) {
+          let subObj = Object.values(obj);
+          strings = [...strings, ...subObj];
+          getStrings(subObj);
         }
-
       }
-      getStrings(valuesx)
+      getStrings(valuesx);
 
-      strings = strings.filter(item => typeof item == 'string' || typeof item == 'number')
+      strings = strings.filter(
+        item => typeof item == "string" || typeof item == "number"
+      );
 
-      return valuesx
+      return valuesx;
     },
     getStrings(obj, valuesx) {
-      let stringsx = Object.values(obj)
-      valuesx.forEach((item) => {
-        if (item && typeof item == 'object') {
-          valuesx = [...valuesx,...Object.values(item)]
+      let stringsx = Object.values(obj);
+      valuesx.forEach(item => {
+        if (item && typeof item == "object") {
+          valuesx = [...valuesx, ...Object.values(item)];
         }
-      })
+      });
       // return [...valuesx,...Object.values(item)]
-      return stringsx
+      return stringsx;
     },
-    changeCheckedMultiple () {
-      let lengthx = this.data.length
-      let lengthSelected = this.value.length
-      let selectedx = (lengthx - lengthSelected)
+    changeCheckedMultiple() {
+      let lengthx = this.data.length;
+      let lengthSelected = this.value.length;
+      let selectedx = lengthx - lengthSelected;
       if (selectedx == 0) {
-        this.$emit('input', [])
+        this.$emit("input", []);
       } else {
-        this.$emit('input', this.data)
+        this.$emit("input", this.data);
       }
     },
-    clicktr (tr, isTr) {
-
-      if(this.multiple && isTr){
-        let val = this.value.slice(0)
-        if(val.includes(tr)) {
-          val.splice(val.indexOf(tr),1)
+    clicktr(tr, isTr) {
+      if (this.multiple && isTr) {
+        let val = this.value.slice(0);
+        if (val.includes(tr)) {
+          val.splice(val.indexOf(tr), 1);
         } else {
-          val.push(tr)
+          val.push(tr);
         }
 
-        this.$emit('input', val)
-        this.$emit('selected', tr)
+        this.$emit("input", val);
+        this.$emit("selected", tr);
       } else if (isTr) {
-        this.$emit('input', tr)
-        this.$emit('selected', tr)
+        this.$emit("input", tr);
+        this.$emit("selected", tr);
       }
     },
-    dblclicktr (tr, isTr) {
-
+    dblclicktr(tr, isTr) {
       if (isTr) {
-        this.$emit('dblSelection',tr)
+        this.$emit("dblSelection", tr);
       }
-
     },
-    listenerChangeWidth () {
-      this.headerWidth = `${this.$refs.table.offsetWidth}px`
-      this.changeTdsWidth()
+    listenerChangeWidth() {
+      this.headerWidth = `${this.$refs.table.offsetWidth}px`;
+      this.changeTdsWidth();
     },
     changeTdsWidth() {
-      if(!this.value) return
+      if (!this.value) return;
 
-      let tbody = this.$refs.table.querySelector('tbody')
+      let tbody = this.$refs.table.querySelector("tbody");
 
       // Adding condition removes querySelector none error - if tbody isnot present
-      if(tbody) {
-        let trvs = tbody.querySelector('.tr-values')
-        if (trvs === undefined || trvs === null ) return
-        let tds = trvs.querySelectorAll('.td')
+      if (tbody) {
+        let trvs = tbody.querySelector(".tr-values");
+        if (trvs === undefined || trvs === null) return;
+        let tds = trvs.querySelectorAll(".td");
 
-        let tdsx = []
+        let tdsx = [];
 
         tds.forEach((td, index) => {
-          tdsx.push({index: index, widthx: td.offsetWidth})
+          tdsx.push({ index: index, widthx: td.offsetWidth });
         });
 
-
-        let colgrouptable = this.$refs.colgrouptable
-        if (colgrouptable !== undefined && colgrouptable !== null ) {
-          let colsTable = colgrouptable.querySelectorAll('.col')
+        let colgrouptable = this.$refs.colgrouptable;
+        if (colgrouptable !== undefined && colgrouptable !== null) {
+          let colsTable = colgrouptable.querySelectorAll(".col");
           colsTable.forEach((col, index) => {
-            col.setAttribute('width', tdsx[index].widthx)
+            col.setAttribute("width", tdsx[index].widthx);
           });
         }
-
       }
     },
-    changeMaxItems (index) {
-      this.maxItemsx = this.descriptionItems[index]
+    changeMaxItems(index) {
+      this.maxItemsx = this.descriptionItems[index];
     }
   }
-}
+};
 </script>
